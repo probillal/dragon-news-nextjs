5edd05a0e885f7c131b01465ba3fe86f6316e43e
probillal <smbillalhossain273@gmail.com>
2026-07-18 11:04:37 +0600

    Logo and header added

--- src/components/shared/Headers.jsx
+++ src/components/shared/Headers.jsx
@@ -1,6 +1,17 @@
+import logo from "@/assets/logo.png";
+import Image from "next/image";
 const Headers = () => {
   return (
-    <div>
+    <div className="text-center py-8 space-y-2">
+      <Image
+        src={logo}
+        alt="Logo"
+        width={300}
+        height={200}
+        className="mx-auto"
+      />
+      <h4>Journalism Without Fear or Favour</h4>
+      <h3>Date</h3>
       <h2>My App Header</h2>
     </div>
   );
