25b7d8207f5d4c5f027bf9192973d591978570d5
probillal <smbillalhossain273@gmail.com>
2026-07-19 23:30:23 +0600

    Update dynamically breaking news and pause on hover added

--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -1,32 +1,37 @@
 import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
+  const news = [
+    {
+      id: 1,
+
+      content:
+        "Artificial Intelligence has become one of the fastest-growing technologies in the world. Companies are investing billions in AI research to improve productivity, healthcare, education, and customer experiences.",
+    },
+    {
+      id: 2,
+
+      content:
+        "The Bangladesh national cricket team delivered an exceptional performance to win the series. Fans celebrated across the country after the thrilling victory.",
+    },
+    {
+      id: 3,
+
+      content:
+        "The Global Climate Summit concluded with countries committing to increased investments in renewable energy, sustainable infrastructure, and carbon reduction initiatives.",
+    },
+  ];
   return (
     <div className="bg-gray-100 py-2 px-4 flex items-center space-x-4 container mx-auto">
       <button className="btn bg-red-500 text-white">Latest News</button>
-      <Marquee>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-        perferendis perspiciatis sapiente eum. Dolores accusantium perspiciatis
-        odit sit porro hic corporis, consequatur architecto aperiam. Sapiente
-        nostrum at asperiores quidem neque vero. Quidem, consequatur laudantium
-        eaque nisi quaerat nostrum atque dolorem quae excepturi error
-        voluptatum, facere possimus cupiditate ex officia voluptatem distinctio!
-        Dolores consequuntur omnis iure! Blanditiis sapiente expedita facilis!
-        Rerum, repudiandae eligendi assumenda inventore ex, cum consequuntur
-        optio esse odit ea, natus animi. Natus aliquid nam quaerat tempore, qui
-        ab suscipit, possimus molestiae omnis, quia culpa ratione officiis at
-        nobis id. Excepturi ratione, incidunt sunt laboriosam neque cumque.
-        Magni ducimus quibusdam asperiores, officiis alias fuga sapiente
-        provident, facere voluptatum natus culpa recusandae beatae? Architecto
-        adipisci tempora, laudantium amet harum enim ipsa rem vero vitae
-        corrupti, mollitia, hic molestiae odit minima accusamus quas culpa! Est
-        in porro iusto ab dolores repellendus eaque asperiores alias culpa
-        nesciunt modi molestiae, itaque temporibus voluptatum ea animi
-        necessitatibus praesentium maiores earum fuga consequatur architecto
-        illum! Saepe eum cumque quod aliquid nisi sed sit ratione in tenetur
-        cupiditate optio officia, labore eligendi reprehenderit consectetur
-        aperiam unde est et mollitia, ipsum id natus dolor? Dicta ex porro, quam
-        quisquam quibusdam autem et voluptas numquam ipsam soluta reprehenderit!
+      <Marquee pauseOnHover={true}>
+        {news.map((n) => {
+          return (
+            <p key={n.id} className="mx-4">
+              {n.content}
+            </p>
+          );
+        })}
       </Marquee>
     </div>
   );
