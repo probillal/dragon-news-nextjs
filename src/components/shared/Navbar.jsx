5892586ccf780cf27992beaa376e36ec2b6e67c3
probillal <smbillalhossain273@gmail.com>
2026-07-21 20:02:51 +0600

    Navbar added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import userAvatar from "@/assets/user.png";
+
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
+    <div className="container mx-auto flex justify-between items-center p-4">
+      <div></div>
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="flex items-center space-x-4">
+        <Image src={userAvatar} alt="User Avatar" width={40} height={40} />
+        <button className="btn btn-primary">Login</button>
+      </div>
     </div>
   );
 };
